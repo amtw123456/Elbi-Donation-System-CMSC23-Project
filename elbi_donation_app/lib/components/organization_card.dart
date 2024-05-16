ea77d99bf6b44438459e8a542279c00852e62d6b
ReddDL <redeleon.dev@gmail.com>
2024-05-16 22:01:40 +0800

    feat: clickable card

--- elbi_donation_app/lib/components/organization_card.dart
+++ elbi_donation_app/lib/components/organization_card.dart
@@ -10,34 +10,37 @@ class OrganizationCard extends StatefulWidget {
 class _OrganizationCardState extends State<OrganizationCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 300,
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.grey,
-          width: 1.0,
-        ),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // image container
-          Container(
-            height: 150,
-            padding: EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(8)
-            ),
+    return GestureDetector(
+      onTap: () {print("clicked");},
+      child: Container(
+        width: double.infinity,
+        height: 300,
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.grey,
+            width: 1.0,
           ),
-          Text('Organization name here', style: TextStyle(fontFamily: "Poppins", fontSize: 20)),
-          Text('A short description here. One or two sentences maybe?'),
-        ],
-      ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // image container
+            Container(
+              height: 150,
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(8)
+              ),
+            ),
+            Text('Organization name here', style: TextStyle(fontFamily: "Poppins", fontSize: 20)),
+            Text('A short description here. One or two sentences maybe?'),
+          ],
+        ),
+      ) 
     );
   }
 }
