7ced26e2b3136464773bdbf87287be4b631602a5
FarrelBeso <vabeso@up.edu.ph>
2024-05-16 11:54:23 +0800

    feat: landing page init

--- elbi_donation_app/lib/views/auth_views/landing.dart
+++ elbi_donation_app/lib/views/auth_views/landing.dart
@@ -6,16 +6,41 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Expanded(
+      body: Container(
+        color: const Color(0xff35aa80),
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Expanded(
+                child: Center(
               child: Column(
-            children: [Text('elBigay'), Text('Make a difference. Together')],
-          )),
-          TextButton(onPressed: () {}, child: const Text('Get Started')),
-          TextButton(
-              onPressed: () {}, child: const Text('I already have an account')),
-        ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'elBigay',
+                    style: TextStyle(fontSize: 48, color: Colors.white),
+                  ),
+                  Text(
+                    'Make a difference. Together.',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  )
+                ],
+              ),
+            )),
+            FilledButton(
+                onPressed: () {},
+                child: const Text(
+                  'Get Started',
+                )),
+            OutlinedButton(
+                onPressed: () {},
+                child: const Text(
+                  'I already have an account',
+                )),
+          ],
+        ),
       ),
     );
   }
