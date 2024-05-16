e68ce9dc55d42734b1d681e04daafde0dea63a35
FarrelBeso <vabeso@up.edu.ph>
2024-05-16 12:01:35 +0800

    feat: initialize adding of sign up page

---
+++ elbi_donation_app/lib/views/auth_views/sign_up_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            const Text('Help us get to know you'),
+            const Text('Create your account so you can start donating.'),
+            const Text('Personal'),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'First name'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Last name'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Username'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Email'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Address'),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Contact number'),
+            ),
+            FilledButton(onPressed: () {}, child: const Text('Sign up')),
+            Row(
+              children: [
+                const Text('Want to sign up as an organization?'),
+                TextButton(onPressed: () {}, child: const Text('Sign up'))
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
