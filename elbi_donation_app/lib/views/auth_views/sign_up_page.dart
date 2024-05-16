03e65ac913a684874873b700de9c032972f849b8
FarrelBeso <vabeso@up.edu.ph>
2024-05-16 12:10:35 +0800

    ui: improved the sign up page

--- elbi_donation_app/lib/views/auth_views/sign_up_page.dart
+++ elbi_donation_app/lib/views/auth_views/sign_up_page.dart
@@ -6,41 +6,59 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(),
         body: SingleChildScrollView(
-      child: Container(
-        child: Column(
-          children: [
-            const Text('Help us get to know you'),
-            const Text('Create your account so you can start donating.'),
-            const Text('Personal'),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'First name'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Last name'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Username'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Email'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Address'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Contact number'),
-            ),
-            FilledButton(onPressed: () {}, child: const Text('Sign up')),
-            Row(
+          child: Container(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text('Want to sign up as an organization?'),
-                TextButton(onPressed: () {}, child: const Text('Sign up'))
+                const Text(
+                  'Help us get to know you',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                ),
+                const Text('Create your account so you can start donating.'),
+                const SizedBox(
+                  height: 48,
+                ),
+                const Text(
+                  'Personal',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'First name'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Last name'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Username'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Email'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Address'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Contact number'),
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: FilledButton(
+                        onPressed: () {}, child: const Text('Sign up'))),
+                Row(
+                  children: [
+                    const Text('Want to sign up as an organization?'),
+                    TextButton(onPressed: () {}, child: const Text('Sign up'))
+                  ],
+                )
               ],
-            )
-          ],
-        ),
-      ),
-    ));
+            ),
+          ),
+        ));
   }
 }
