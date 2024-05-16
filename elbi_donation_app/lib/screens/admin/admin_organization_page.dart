3d5dd14031c5c7a9daca39150d3b41f5abdaa833
ReddDL <redeleon.dev@gmail.com>
2024-05-16 14:07:25 +0800

    feat: create organization page

---
+++ elbi_donation_app/lib/screens/admin/admin_organization_page.dart
@@ -0,0 +1,47 @@
+import 'package:elbi_donation_app/components/organization_card.dart';
+import 'package:elbi_donation_app/screens/admin/admin_nav_bar.dart';
+import 'package:flutter/material.dart';
+
+class AdminOrganizationPage extends StatefulWidget {
+  const AdminOrganizationPage({super.key});
+
+  @override
+  State<AdminOrganizationPage> createState() => _AdminOrganizationPageState();
+}
+
+class _AdminOrganizationPageState extends State<AdminOrganizationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF8F8F8),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hello, admin 👋", style: TextStyle(fontFamily: 'Poppins', fontSize: 32, fontWeight: FontWeight.w700, color: Color(0xFF37A980))),
+            Text("Here are the organizations that are accepting donations", style: TextStyle(fontFamily: 'Poppins', fontSize: 16)),
+            SizedBox(height: 25),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) => SizedBox(height: 25),
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return OrganizationCard();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
