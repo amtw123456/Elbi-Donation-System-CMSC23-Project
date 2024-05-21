214c1f3c5c32325dbd0ee2be49d686e02827e47f
ReddDL <redeleon.dev@gmail.com>
2024-05-21 23:48:10 +0800

    feat: create org navbar

---
+++ elbi_donation_app/lib/components/org_navbar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class OrgNavbar extends StatefulWidget {
+  final ValueChanged<int> onItemTapped;
+  final int selectedIndex;
+
+  const OrgNavbar({
+    Key? key,
+    required this.onItemTapped,
+    required this.selectedIndex,
+  }) : super(key: key);
+
+  @override
+  State<OrgNavbar> createState() => _OrgNavbarState();
+}
+
+class _OrgNavbarState extends State<OrgNavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Color(0xFF37A980),
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.volunteer_activism_rounded),
+          label: 'Donations',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.card_giftcard_rounded),
+          label: 'Drives',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: widget.selectedIndex,
+      onTap: widget.onItemTapped, // Use widget.onItemTapped here
+    );
+  }
+}
