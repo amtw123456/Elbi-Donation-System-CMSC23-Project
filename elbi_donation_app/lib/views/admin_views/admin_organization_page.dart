bc628312c1cfa3df8fcb5551ff2edf854e4fae51
FarrelBeso <vabeso@up.edu.ph>
2024-05-25 02:49:55 +0800

    chore: integrate the org info to ui

--- elbi_donation_app/lib/views/admin_views/admin_organization_page.dart
+++ elbi_donation_app/lib/views/admin_views/admin_organization_page.dart
@@ -1,8 +1,11 @@
 import 'package:elbi_donation_app/components/organization_card.dart';
+import 'package:elbi_donation_app/providers/user_provider.dart';
 import 'package:elbi_donation_app/views/admin_views/admin_nav_bar.dart';
 import 'package:elbi_donation_app/views/admin_views/admin_org_details.dart';
 import 'package:elbi_donation_app/views/admin_views/admin_signups_page.dart';
+import 'package:elbi_donation_app/views/user_views/user_organization_donation_details_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AdminOrganizationPage extends StatefulWidget {
   const AdminOrganizationPage({super.key});
@@ -14,56 +17,76 @@ class AdminOrganizationPage extends StatefulWidget {
 class _AdminOrganizationPageState extends State<AdminOrganizationPage> {
   @override
   Widget build(BuildContext context) {
+    final futureOrgList = context.read()<UserProvider>().getOrganizations();
     return Scaffold(
-      backgroundColor: Color(0xFFF8F8F8),
+      backgroundColor: const Color(0xFFF8F8F8),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 25),
-            Text("Hello, admin 👋", style: TextStyle(fontFamily: 'Poppins', fontSize: 32, fontWeight: FontWeight.w700, color: Color(0xFF37A980))),
-            Text("Here are the organizations that are accepting donations", style: TextStyle(fontFamily: 'Poppins', fontSize: 16)),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => AdminSignups()));
-              },
-              child: Container(
-                  width: double.infinity,
-                  child: Padding(
-                    child: Text("Pending sign ups", style: TextStyle(fontFamily: "Poppins", fontSize: 16),),
-                    padding: EdgeInsets.all(15),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                ),
-              ),
+            const SizedBox(height: 40),
+            const Text("Hello, admin 👋",
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF37A980))),
+            const Text("Here are some organizations that might interest you",
+                style: TextStyle(fontFamily: 'Poppins', fontSize: 16)),
+            const SizedBox(
+              height: 20,
             ),
-            
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    ListView.separated(
-                      separatorBuilder: (BuildContext context, int index) => SizedBox(height: 25),
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => AdminOrganizationDetails())); 
-                          },
-                          child: OrganizationCard()
-                        ) ;
-                      },
-                    ),
+                    FutureBuilder<dynamic>(
+                        future: futureOrgList,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            if (snapshot.data!['success']) {
+                              final organizations = snapshot.data!['orgs'];
+                              return ListView.separated(
+                                separatorBuilder:
+                                    (BuildContext context, int index) =>
+                                        const SizedBox(height: 25),
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: organizations.length,
+                                itemBuilder: (context, index) {
+                                  return GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    UserOrganizationDetails(
+                                                      organization:
+                                                          organizations[index],
+                                                    )));
+                                      },
+                                      child: OrganizationCard(
+                                          organization: organizations[index]));
+                                },
+                              );
+                            } else {
+                              return const Text('No organizations yet!');
+                            }
+                          } else if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          } else {
+                            return const SizedBox(
+                              width: 60,
+                              height: 60,
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                        })
                   ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
