cd6a038f9fdb52afd4bbf78937fb9a3d101938d2
ReddDL <redeleon.dev@gmail.com>
2024-05-25 17:34:07 +0800

    fix: fixed routing

--- elbi_donation_app/lib/components/navigation_helper.dart
+++ elbi_donation_app/lib/components/navigation_helper.dart
@@ -2,7 +2,9 @@
 
 import 'package:elbi_donation_app/models/user_model.dart';
 import 'package:elbi_donation_app/views/admin_views/admin_profile.dart';
+import 'package:elbi_donation_app/views/admin_views/admin_router.dart';
 import 'package:elbi_donation_app/views/org_views/org_home_page.dart';
+import 'package:elbi_donation_app/views/org_views/org_router.dart';
 import 'package:elbi_donation_app/views/user_views/user_router.dart';
 import 'package:flutter/material.dart';
 
@@ -21,11 +23,11 @@ class NavigationHelper extends StatelessWidget {
               // then route depending on user type
               UserModel userModel = snapshot.data!['userModel'];
               if (userModel.type == 'org') {
-                return const OrgHomePage();
+                return const OrgRouter();
               } else if (userModel.type == 'donor') {
                 return const UserRouter();
               } else if (userModel.type == 'admin') {
-                return const AdminProfile();
+                return const AdminRouter();
               } else {
                 return const Scaffold(
                   body: Center(
