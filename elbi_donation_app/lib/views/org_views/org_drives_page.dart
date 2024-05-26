c0539ddd005896bcb9c6eda8dbf4fcafdcc3951c
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-05-26 17:32:04 +0800

    feat: made donationdrives page use future builder

--- elbi_donation_app/lib/views/org_views/org_drives_page.dart
+++ elbi_donation_app/lib/views/org_views/org_drives_page.dart
@@ -18,6 +18,8 @@ class OrgDonationDrivePage extends StatefulWidget {
 class OrgDonationDrivePageState extends State<OrgDonationDrivePage> {
   @override
   Widget build(BuildContext context) {
+    final userId = context.read<UserAuthProvider>().user?.uid;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -31,32 +33,50 @@ class OrgDonationDrivePageState extends State<OrgDonationDrivePage> {
         centerTitle: true,
         automaticallyImplyLeading: false,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              ListView.separated(
-                separatorBuilder: (BuildContext context, int index) =>
-                    SizedBox(height: 25),
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    child: DonationDriveCard(),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => OrgDonationDriveDetails()));
-                    },
-                  );
-                },
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: context.read<UserProvider>().getUserModel(userId!),
+        builder: (BuildContext context,
+            AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            final userInformation = snapshot.data!;
+            final organizationDriveIds =
+                userInformation['userModel'].organizationDriveList;
+            return SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) =>
+                          SizedBox(height: 25),
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: organizationDriveIds.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          child: DonationDriveCard(),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        OrgDonationDriveDetails()));
+                          },
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
+            );
+          } else {
+            return Text('No data available');
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
