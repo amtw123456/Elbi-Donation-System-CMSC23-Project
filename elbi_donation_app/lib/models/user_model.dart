b5c30cf57d7870afe40fb54ca1dfc68caa8758eb
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-05-26 16:09:17 +0800

    feat: added organizationDonationDrivesList

--- elbi_donation_app/lib/models/user_model.dart
+++ elbi_donation_app/lib/models/user_model.dart
@@ -5,46 +5,52 @@ class UserModel {
   String? firstName;
   String? lastName;
   String? username;
-  String email;
+  String? email;
   String? address;
   String? contactNumber;
-  // this could be 'admin', 'org', or 'donor'
-  String? type; // this refers to the user type
-  // for orgs only
-  bool? isApprovedByAdmin; // approval first if they're an org
+  String? type;
+  bool? isApprovedByAdmin;
   String? orgName;
-  String? status; // status for donations if open or close
-  String? orgDescription; // description of the org
+  String? status;
+  String? orgDescription;
+  List<String>
+      organizationDonationDrivesList; // Adjusted to have a default empty list
 
-  UserModel(
-      {this.id,
-      this.firstName,
-      this.lastName,
-      this.username,
-      this.orgName,
-      required this.email,
-      this.address,
-      this.contactNumber,
-      this.type,
-      this.isApprovedByAdmin,
-      this.status,
-      this.orgDescription});
+  UserModel({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.username,
+    this.orgName,
+    required this.email,
+    this.address,
+    this.contactNumber,
+    this.type,
+    this.isApprovedByAdmin,
+    this.status,
+    List<String>?
+        organizationDonationDrivesList, // Updated parameter to accept null
+    this.orgDescription,
+  }) : organizationDonationDrivesList =
+            organizationDonationDrivesList ?? []; // Set default value
 
-  // Factory constructor to instantiate object from json format
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-        id: json['id'],
-        firstName: json['firstName'],
-        lastName: json['lastName'],
-        username: json['username'],
-        orgName: json['orgName'],
-        email: json['email'],
-        address: json['address'],
-        contactNumber: json['contactNumber'],
-        type: json['type'],
-        isApprovedByAdmin: json['isApprovedByAdmin'],
-        status: json['status'],
-        orgDescription: json['orgDescription']);
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      username: json['username'],
+      orgName: json['orgName'],
+      email: json['email'],
+      address: json['address'],
+      contactNumber: json['contactNumber'],
+      type: json['type'],
+      isApprovedByAdmin: json['isApprovedByAdmin'],
+      status: json['status'],
+      orgDescription: json['orgDescription'],
+      organizationDonationDrivesList:
+          List<String>.from(json['organizationDonationDrivesList'] ?? []),
+    );
   }
 
   static List<UserModel> fromJsonArray(String jsonData) {
@@ -52,20 +58,21 @@ class UserModel {
     return data.map<UserModel>((dynamic d) => UserModel.fromJson(d)).toList();
   }
 
-  Map<String, dynamic> toJson(UserModel userModel) {
+  Map<String, dynamic> toJson() {
     return {
-      'id': userModel.id,
-      'firstName': userModel.firstName,
-      'lastName': userModel.lastName,
-      'username': userModel.username,
-      'orgName': userModel.orgName,
-      'email': userModel.email,
-      'address': userModel.address,
-      'contactNumber': userModel.contactNumber,
-      'type': userModel.type,
-      'isApprovedByAdmin': userModel.isApprovedByAdmin,
-      'status': userModel.status,
-      'orgDescription': userModel.orgDescription
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'username': username,
+      'orgName': orgName,
+      'email': email,
+      'address': address,
+      'contactNumber': contactNumber,
+      'type': type,
+      'isApprovedByAdmin': isApprovedByAdmin,
+      'status': status,
+      'orgDescription': orgDescription,
+      'organizationDonationDrivesList': organizationDonationDrivesList,
     };
   }
 }
