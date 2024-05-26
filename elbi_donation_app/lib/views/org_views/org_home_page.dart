48dd98fa2f350259e3111506bd35d735a73ee033
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-05-26 15:23:50 +0800

    feat: integrated org views fetch user details

--- elbi_donation_app/lib/views/org_views/org_home_page.dart
+++ elbi_donation_app/lib/views/org_views/org_home_page.dart
@@ -1,6 +1,9 @@
 import 'package:elbi_donation_app/components/donation_card.dart';
 import 'package:elbi_donation_app/views/org_views/org_donation_details_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:elbi_donation_app/providers/user_provider.dart';
+import 'package:elbi_donation_app/providers/auth_provider.dart';
 import 'package:elbi_donation_app/views/user_views/user_organization_donation_details_page.dart';
 
 import '../../components/organization_card.dart';
@@ -16,55 +19,73 @@ class _OrgHomePageState extends State<OrgHomePage> {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    final userId = context.read<UserAuthProvider>().user?.uid;
 
     return Scaffold(
       backgroundColor: Color(0xFFF8F8F8),
       body: Padding(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 40),
-            Text("Hello, userName 👋",
-                style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 32,
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xFF37A980))),
-            Text("Here are the recent donations made by users:",
-                style: TextStyle(fontFamily: 'Poppins', fontSize: 16)),
-            SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ListView.separated(
-                      padding: EdgeInsets.zero,
-                      separatorBuilder: (BuildContext context, int index) =>
-                          SizedBox(height: 25),
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        OrgDonationDetails()));
-                          },
-                          child: DonationCard(),
-                        );
-                      },
+        child: FutureBuilder<Map<String, dynamic>>(
+          future: context.read<UserProvider>().getUserModel(userId!),
+          builder: (BuildContext context,
+              AsyncSnapshot<Map<String, dynamic>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              final userInformation = snapshot.data!;
+              final userName = userInformation['userModel'].username;
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 40),
+                  Text("Hello, $userName 👋",
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF37A980))),
+                  Text("Here are the recent donations made by users:",
+                      style: TextStyle(fontFamily: 'Poppins', fontSize: 16)),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ListView.separated(
+                            padding: EdgeInsets.zero,
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    SizedBox(height: 25),
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: 5,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              OrgDonationDetails()));
+                                },
+                                child: DonationCard(),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+                  ),
+                ],
+              );
+            } else {
+              return Text('No data available');
+            }
+          },
         ),
       ),
     );
