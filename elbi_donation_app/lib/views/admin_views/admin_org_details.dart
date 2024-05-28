0c2f625ab75fda128dc60377e456aa11d4450bd1
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-05-29 00:30:48 +0800

    feat: added new button on admin org details page for verifying organizations

--- elbi_donation_app/lib/views/admin_views/admin_org_details.dart
+++ elbi_donation_app/lib/views/admin_views/admin_org_details.dart
@@ -16,101 +16,128 @@ class AdminOrganizationDetailsState extends State<AdminOrganizationDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          title: const Text('Details',
-              style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700)),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
-        body: Padding(
-            padding: EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 25),
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Image.network(
-                    widget.organization.proofOfLegitimacyImageUrlLink!,
-                    fit: BoxFit
-                        .cover, // Make the image cover the entire container
+        title: const Text('Details',
+            style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 24,
+                fontWeight: FontWeight.w700)),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 25),
+            Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(8)),
+              child: Image.network(
+                widget.organization.proofOfLegitimacyImageUrlLink!,
+                fit: BoxFit.cover, // Make the image cover the entire container
+              ),
+            ),
+            Text(widget.organization.orgName!,
+                style: const TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF37A980))),
+            Expanded(
+              child: widget.organization.orgDescription != null
+                  ? Text(
+                      widget.organization.orgDescription!,
+                    )
+                  : Text("No description yet!"),
+            ),
+            Column(children: [
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(10),
+                      backgroundColor: Color(0xFF37A980),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      )),
+                  onPressed: () {},
+                  child: const Text(
+                    'Verify',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Poppins",
+                        fontSize: 20),
                   ),
                 ),
-                Text(widget.organization.orgName!,
-                    style: const TextStyle(
-                        fontFamily: "Poppins",
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                        color: Color(0xFF37A980))),
-                Expanded(
-                    child: widget.organization.orgDescription != null
-                        ? Text(
-                            widget.organization.orgDescription!,
-                          )
-                        : Text("No description yet!")),
-                SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.all(10),
-                            backgroundColor: Color(0xFF37A980),
-                            shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5)),
-                            )),
-                        onPressed: () {
-                          showModalBottomSheet(
-                            context: context,
-                            isScrollControlled: true,
-                            backgroundColor: Colors.transparent,
-                            builder: (context) => Container(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.75,
-                                decoration: new BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: new BorderRadius.only(
-                                    topLeft: const Radius.circular(25.0),
-                                    topRight: const Radius.circular(25.0),
-                                  ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(10),
+                      backgroundColor: Color(0xFF37A980),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      )),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (context) => Container(
+                        height: MediaQuery.of(context).size.height * 0.75,
+                        decoration: new BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: new BorderRadius.only(
+                            topLeft: const Radius.circular(25.0),
+                            topRight: const Radius.circular(25.0),
+                          ),
+                        ),
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: EdgeInsets.all(30),
+                            child: Column(
+                              children: [
+                                ListView.separated(
+                                  separatorBuilder:
+                                      (BuildContext context, int index) =>
+                                          SizedBox(height: 25),
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemCount: 5,
+                                  itemBuilder: (context, index) {
+                                    return DonationCard();
+                                  },
                                 ),
-                                child: SingleChildScrollView(
-                                    child: Padding(
-                                  padding: EdgeInsets.all(30),
-                                  child: Column(
-                                    children: [
-                                      ListView.separated(
-                                        separatorBuilder:
-                                            (BuildContext context, int index) =>
-                                                SizedBox(height: 25),
-                                        shrinkWrap: true,
-                                        physics: NeverScrollableScrollPhysics(),
-                                        itemCount: 5,
-                                        itemBuilder: (context, index) {
-                                          return DonationCard();
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                ))),
-                          );
-                        },
-                        child: const Text('View Donations',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: "Poppins",
-                                fontSize: 20)))),
-              ],
-            )));
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'View Donations',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Poppins",
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
   }
 }
