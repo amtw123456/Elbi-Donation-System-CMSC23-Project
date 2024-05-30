eeb6574d7b7b465c7f569cfac9d6bdcc58dc58b4
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-05-31 02:19:31 +0800

    feat: organization page allow organization profile picture

--- elbi_donation_app/lib/views/org_views/org_profile.dart
+++ elbi_donation_app/lib/views/org_views/org_profile.dart
@@ -4,6 +4,11 @@ import '../../providers/auth_provider.dart';
 import 'package:elbi_donation_app/providers/user_provider.dart';
 import 'package:elbi_donation_app/views/auth_views/landing.dart';
 
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'dart:io';
+
 class OrgProfile extends StatefulWidget {
   const OrgProfile({super.key});
 
@@ -16,6 +21,11 @@ class OrgProfileState extends State<OrgProfile> {
   List<String> statusesForDonation = ['Open', 'Close'];
 
   bool _isLoading = false;
+
+  File? _selectedImage;
+
+  XFile? file;
+  String imageUrl = '';
 
   @override
   Widget build(BuildContext context) {
@@ -90,72 +100,295 @@ class OrgProfileState extends State<OrgProfile> {
                               );
                             }).toList(),
                           ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            'Upload Organization Cover Photo',
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          // UPLOAD IMAGE
+                          SizedBox(
+                            width: double.infinity,
+                            height: 150,
+                            child: _selectedImage != null
+                                ? Image.file(_selectedImage!)
+                                : ElevatedButton(
+                                    onPressed: () async {
+                                      if (await Permission.storage
+                                          .request()
+                                          .isGranted) {
+                                        // _pickImageFromGallery();
+                                        file = await ImagePicker().pickImage(
+                                            source: ImageSource.gallery);
+                                        setState(() {});
+                                      } else {
+                                        // Permission is not granted. Handle the scenario accordingly.
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: Text(
+                                                  "storage Permission Required"),
+                                              content: Text(
+                                                  "Please grant stroage permission in settings to enable storage access."),
+                                              actions: <Widget>[
+                                                ElevatedButton(
+                                                  child: Text("CANCEL"),
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                ),
+                                                ElevatedButton(
+                                                  child: Text("SETTINGS"),
+                                                  onPressed: () {
+                                                    openAppSettings(); // This will open the app settings where the user can enable permissions.
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      }
+                                    },
+                                    child: Center(
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .center, // Center the contents vertically
+                                        children: [
+                                          Icon(Icons.photo_library,
+                                              size: 40,
+                                              color: Colors.grey[
+                                                  500]), // Adjust size and color as needed
+                                          SizedBox(
+                                              height:
+                                                  10), // Add some space between the icon and the text
+                                          Text(
+                                            "Upload photo",
+                                            style: TextStyle(
+                                                color: Colors.grey[500],
+                                                fontSize: 16,
+                                                fontFamily:
+                                                    'Poppins'), // Adjust text style as needed
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      elevation: 0,
+                                      padding: const EdgeInsets.all(20),
+                                      backgroundColor: Colors.grey[50],
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        side: BorderSide(
+                                            color: const Color(0xFF37A980),
+                                            width: 2),
+                                      ),
+                                    ),
+                                  ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('or'),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          // TAKE IMAGE
+                          SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.camera_alt,
+                                        color: Color(0xFF37A980)),
+                                    SizedBox(width: 5),
+                                    Text(
+                                      'Open Camera & Take Photo',
+                                      style: TextStyle(
+                                          color: Color(0xFF37A980),
+                                          fontFamily: 'Poppins'),
+                                    ),
+                                  ],
+                                ),
+                                onPressed: () async {
+                                  if (await Permission.camera
+                                      .request()
+                                      .isGranted) {
+                                    file = await ImagePicker()
+                                        .pickImage(source: ImageSource.camera);
+                                    setState(() {});
+                                  } else {
+                                    // Permission is not granted. Handle the scenario accordingly.
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text(
+                                              "Camera Permission Required"),
+                                          content: Text(
+                                              "Please grant camera permission in settings to enable camera access."),
+                                          actions: <Widget>[
+                                            ElevatedButton(
+                                              child: Text("CANCEL"),
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                            ),
+                                            ElevatedButton(
+                                              child: Text("SETTINGS"),
+                                              onPressed: () {
+                                                openAppSettings(); // This will open the app settings where the user can enable permissions.
+                                              },
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    elevation: 0,
+                                    padding: const EdgeInsets.all(20),
+                                    backgroundColor: Colors.green[50],
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    )),
+                              )),
                         ],
                       ),
                       SizedBox(height: 10),
+                      file != null
+                          ? Image.file(File(file!.path))
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Please select an image",
+                                    style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        color: Colors.red))
+                              ],
+                            ),
                       SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  padding: EdgeInsets.all(10),
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.all(10),
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                              )),
+                          onPressed: () async {
+                            try {
+                              // start loading
+                            } catch (e) {
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(e.toString()),
+                                  backgroundColor: Colors.green,
+                                ));
+                              }
+
+                              setState(() {
+                                _isLoading = false;
+                              });
+                            }
+                          },
+                          child: _isLoading
+                              ? Container(
+                                  width: 20,
+                                  height: 20,
+                                  padding: const EdgeInsets.all(4),
+                                  child: const CircularProgressIndicator(
+                                    color: Colors.white,
+                                    strokeWidth: 2,
+                                  ),
+                                )
+                              : const Text(
+                                  'Update Profile',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "Poppins",
+                                      fontSize: 20),
+                                ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.all(10),
+                              backgroundColor: Colors.red,
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                              )),
+                          onPressed: () async {
+                            try {
+                              // start loading
+                              setState(() {
+                                _isLoading = true;
+                              });
+                              final result = await context
+                                  .read<UserAuthProvider>()
+                                  .signOut();
+                              if (!result['success']) {
+                                throw result['error'];
+                              }
+
+                              if (context.mounted) {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => const LandingPage()));
+                              }
+
+                              // end loading
+                              setState(() {
+                                _isLoading = false;
+                              });
+                            } catch (e) {
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(e.toString()),
                                   backgroundColor: Colors.red,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(5)),
-                                  )),
-                              onPressed: () async {
-                                try {
-                                  // start loading
-                                  setState(() {
-                                    _isLoading = true;
-                                  });
-                                  final result = await context
-                                      .read<UserAuthProvider>()
-                                      .signOut();
-                                  if (!result['success']) {
-                                    throw result['error'];
-                                  }
+                                ));
+                              }
 
-                                  if (context.mounted) {
-                                    Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const LandingPage()));
-                                  }
-
-                                  // end loading
-                                  setState(() {
-                                    _isLoading = false;
-                                  });
-                                } catch (e) {
-                                  if (context.mounted) {
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(SnackBar(
-                                      content: Text(e.toString()),
-                                      backgroundColor: Colors.red,
-                                    ));
-                                  }
-
-                                  setState(() {
-                                    _isLoading = false;
-                                  });
-                                }
-                              },
-                              child: _isLoading
-                                  ? Container(
-                                      width: 20,
-                                      height: 20,
-                                      padding: const EdgeInsets.all(4),
-                                      child: const CircularProgressIndicator(
-                                        color: Colors.white,
-                                        strokeWidth: 2,
-                                      ),
-                                    )
-                                  : const Text('Log out',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontFamily: "Poppins",
-                                          fontSize: 20)))),
+                              setState(() {
+                                _isLoading = false;
+                              });
+                            }
+                          },
+                          child: _isLoading
+                              ? Container(
+                                  width: 20,
+                                  height: 20,
+                                  padding: const EdgeInsets.all(4),
+                                  child: const CircularProgressIndicator(
+                                    color: Colors.white,
+                                    strokeWidth: 2,
+                                  ),
+                                )
+                              : const Text(
+                                  'Log out',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: "Poppins",
+                                      fontSize: 20),
+                                ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
